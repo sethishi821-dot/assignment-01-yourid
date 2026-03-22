77d30cea5d923233d1ed30f20127d658fc8ed282
sethishi821-dot <sethishi821@gmail.com>
2026-03-22 18:49:41 +0530

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,1 +1,14 @@
+-- Get all customers
+SELECT * FROM Customers;
 
+-- Get all products
+SELECT * FROM Products;
+
+-- Join example (Orders with Customers)
+SELECT 
+    Customers.name,
+    Orders.order_id,
+    Orders.order_date
+FROM Orders
+JOIN Customers 
+ON Orders.customer_id = Customers.customer_id;
